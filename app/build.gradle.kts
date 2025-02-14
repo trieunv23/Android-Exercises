66750809920ef323e86789277ddbaac09ef12375
trieunv23 <trieunv.23it@vku.udn.vn>
2025-02-15 02:59:07 +0700

    commit_for_week5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.testkotlin"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
